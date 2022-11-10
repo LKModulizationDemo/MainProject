1b84341721ff4a61685c8d71c9d234fe0241308f
likang <972701125@qq.com>
2022-11-10 16:40:38 +0800

    no message

---
+++ MainProject/MainProject/BViewController/Targets/Target_B.m
@@ -0,0 +1,20 @@
+//
+//  Target_B.m
+//  MainProject
+//
+//  Created by xiuyuan on 2022/11/10.
+//  Copyright © 2022 casa. All rights reserved.
+//
+
+#import "Target_B.h"
+#import "BViewController.h"
+
+@implementation Target_B
+
+- (UIViewController *)Action_viewController:(NSDictionary *)params
+{
+    NSString *contentText = params[@"contentText"];
+    BViewController *viewController = [[BViewController alloc] initWithContentText:contentText];
+    return viewController;
+}
+@end
---
+++ MainProject/Pods/Headers/Public/A/Target_A.h
@@ -0,0 +1,1 @@
+../../../../../A/A/A/Classes/Target/Target_A.h
